53e9d0119306fad49d61ba87151cdebe25ae46c2
Zain-Haider-ML <hzain8206@gmail.com>
2024-07-29 03:04:42 +0200

    prepare base model added

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Chest-Cancer-Classification-End-to-End-using-MLflow-DVC
+
+
+##Workflows
+
+Update config.yaml
+Update secrets.yaml [Optional]
+Update params.yaml
+Update the entity
+Update the configuration manager in src config
+Update the components
+Update the pipeline
+Update the main.py
+Update the dvc.yaml
--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 from FineTunedClassifier import logger
+from src.FineTunedClassifier.pipeline.stage_01_prepare_base_model import PrepareBaseModelTrainingPipeline
 
-logger.info("Welcome to Main.py")
+
+
+
+STAGE_NAME = "Prepare base model"
+
+
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
